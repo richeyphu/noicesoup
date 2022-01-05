a0bd06a3c3c53a9db46cc3250cb50aa1e30d8b92
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-01-05 15:18:27 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-  name='noicesoup',
-  version='0.1.0',
-  description = 'A simple python library for scraping and downloading image from Google',
-  url='https://github.com/richeyphu/noicesoup',
-  author='Unn&Phu',
-  author_email='example@gmail.com',
-  license='MIT',
-  packages=['noicesoup'],
-  install_requires=['requests', 'bs4' , 'selenium'],
+    name='noicesoup',
+    version='0.1.0',
+    description='A simple python package for scraping and downloading images from Google',
+    url='https://github.com/richeyphu/noicesoup',
+    author='Unn&Phu',
+    author_email='example@gmail.com',
+    license='MIT',
+    packages=['noicesoup'],
+    install_requires=['requests', 'bs4', 'selenium'],
 )
