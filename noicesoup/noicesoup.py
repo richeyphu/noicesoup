41d84122510dbeabbd07ad61e48c4af14b88ee7f
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-01-05 15:26:28 +0700

    Create noicesoup.py

---
+++ noicesoup/noicesoup.py
@@ -0,0 +1,19 @@
+"""
+A simple python package for scraping and downloading images from Google
+"""
+
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+# import time
+# import requests
+# import urllib.request
+# import os
+# import sys
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == '__main__':
+    main()
